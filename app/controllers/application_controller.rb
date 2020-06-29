3de1d3937898b9955303ffa2a68eb20363b9fc3d
tanhongit <phuongtan12357nguyen@gmail.com>
2020-06-29 21:11:29 +0700

    add admin Limiting Access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
+  protected
+
+    def authorize
+        unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+        end
+    end
 end
