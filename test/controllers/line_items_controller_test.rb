f679c43822779397487045ad358c4a2000f4101f
tanhongit <phuongtan12357nguyen@gmail.com>
2020-06-29 14:47:04 +0700

    add total cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -16,11 +16,18 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create line_item" do
-    assert_difference('LineItem.count') do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
-    end
+    # assert_difference('LineItem.count') do
+    #   post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    # end
 
-    assert_redirected_to line_item_url(LineItem.last)
+    # assert_redirected_to line_item_url(LineItem.last)
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id }
+      end
+      follow_redirect!
+      
+      assert_select 'h2', 'Your Cart'
+      assert_select 'td', "Programming Ruby 1.9"
   end
 
   test "should show line_item" do
