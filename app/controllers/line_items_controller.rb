b2f48371abebe9b86aae3eee2a60da89e225aee6
tanhongit <phuongtan12357nguyen@gmail.com>
2020-06-29 15:27:18 +0700

    add ajax cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -34,6 +34,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
