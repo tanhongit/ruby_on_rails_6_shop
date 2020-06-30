3ae56ea696b414bebf5604e1dfa1e25fde802843
tanhongit <phuongtan12357nguyen@gmail.com>
2020-06-30 10:58:12 +0700

    edit authorize and logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,15 @@ class ApplicationController < ActionController::Base
   protected
 
   def authorize
-    unless User.find_by(id: session[:user_id])
-      redirect_to login_url, notice: "Please log in"
+    if request.format == Mime[:html]
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    else
+      authenticate_or_request_with_http_basic do |username, password|
+        user = User.find_by(name: username)
+        user && user.authenticate(password)
+      end
     end
   end
 
