97543c30df5419cba77ae01b02da1069da1484bd
tanhongit <phuongtan12357nguyen@gmail.com>
2020-06-30 11:25:12 +0700

    mailboxes

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,20 +1,21 @@
+#---
+# Excerpted from "Agile Web Development with Rails 6",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
+#---
 class SupportMailbox < ApplicationMailbox
   def process
-    puts "START SupportMailbox#process:"
-    puts "From : #{mail.from_address}"
-    puts "Subject: #{mail.subject}"
-    puts "Body : #{mail.body}"
-    puts "END SupportMailbox#process:"
-
-    recent_order = Order.where(email: mail.from_address.to_s).order('created_at desc').first
+    recent_order = Order.where(email: mail.from_address.to_s).
+                         order('created_at desc').
+                         first
     SupportRequest.create!(
-        email:
-            mail.from_address.to_s,
-        subject: mail.subject,
-        body:
-            mail.body.to_s,
-        order:
-            recent_order
+      email: mail.from_address.to_s,
+      subject: mail.subject,
+      body: mail.body.to_s,
+      order: recent_order
     )
   end
 end
