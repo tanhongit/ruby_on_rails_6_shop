a587ec1f33365d693b87dba71530d23d157f068b
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-02 09:00:50 +0700

    edit react task

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -12,8 +12,12 @@ class PayTypeSelector extends React.Component {
     this.state = { selectedPayType: null };
   }
 
+  // onPayTypeSelected(event) {
+  //   this.setState({ selectedPayType: event.target.value });
+  // }
+
   onPayTypeSelected(event) {
-    this.setState({ selectedPayType: event.target.value });
+    console.log(event.target.value);
   }
 
   render() {
