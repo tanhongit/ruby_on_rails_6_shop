326a7b3d4e21afe17521d9bd9a047acb814d9643
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-02 11:47:30 +0700

    edit cart nil

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,7 +7,11 @@ class CartsController < ApplicationController
   # GET /carts
   # GET /carts.json
   def index
-    @carts = Cart.all
+    # @carts = Cart.all
+    @carts = []
+        if session[:cart_id] != nil
+            @carts << Cart.find_by_id(session[:cart_id])
+        end
   end
 
   # GET /carts/1
